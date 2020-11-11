00bb6fc7ba8cd54cee8f4bacf2ce41fb50c5252b
xiami <xiami@amazon.com>
2020-11-10 18:00:06 -0800

    add hostogram example

--- generator/src/main/java/com/amazon/cloudwatch/otel/metrics/MetricGenerator.java
+++ generator/src/main/java/com/amazon/cloudwatch/otel/metrics/MetricGenerator.java
@@ -1,39 +1,26 @@
 package com.amazon.cloudwatch.otel.metrics;
 
 import com.google.common.base.Strings;
-import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.opentelemetry.OpenTelemetry;
 import io.opentelemetry.common.Labels;
-import io.opentelemetry.context.Scope;
-import io.opentelemetry.context.propagation.DefaultContextPropagators;
 import io.opentelemetry.exporters.otlp.OtlpGrpcMetricExporter;
-import io.opentelemetry.exporters.otlp.OtlpGrpcSpanExporter;
-import io.opentelemetry.extensions.trace.propagation.AwsXRayPropagator;
 import io.opentelemetry.metrics.LongCounter;
 import io.opentelemetry.metrics.LongValueRecorder;
 import io.opentelemetry.metrics.Meter;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
-import io.opentelemetry.sdk.extensions.trace.aws.AwsXRayIdsGenerator;
 import io.opentelemetry.sdk.metrics.export.IntervalMetricReader;
 import io.opentelemetry.sdk.metrics.export.MetricExporter;
-import io.opentelemetry.sdk.resources.Resource;
-import io.opentelemetry.sdk.trace.TracerSdkProvider;
-import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
-import io.opentelemetry.sdk.trace.export.SpanExporter;
-import io.opentelemetry.trace.Span;
-import io.opentelemetry.trace.Status;
-import io.opentelemetry.trace.Tracer;
+
 
 import java.util.Collections;
-import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class MetricGenerator {
 
     private static final String OTLP_ENDPOINT = "otlp_endpoint";
     private static final String OTLP_INSTANCE_ID = "otlp_instance_id";
-    private static String otlpEndpoint = "127.0.0.1:55680";
+    private static String otlpEndpoint = "localhost:55680";
 
     static {
         if (!Strings.isNullOrEmpty(System.getenv(OTLP_ENDPOINT))) {
@@ -80,6 +67,12 @@ public class MetricGenerator {
           .setUnit("1")
           .build();
 
+        LongValueRecorder apiLatencyRecorder = meter
+            .longValueRecorderBuilder("latency")
+            .setDescription("API latency time")
+            .setUnit("ms")
+            .build();
+
         // It is recommended that the API user keep a reference to a Bound Counter for the entire time or
         // call unbind when no-longer needed.
         LongCounter.BoundLongCounter someWorkCounter = counter.bind(Labels.of("Key", "SomeWork"));
@@ -90,7 +83,9 @@ public class MetricGenerator {
 
             // Alternatively, the user can use the unbounded counter and explicitly
             // specify the labels set at call-time:
-            counter.add(123, Labels.of("Key", "SomeWork"));
+//            counter.add(123, Labels.of("Key", "SomeWork"));
+
+            apiLatencyRecorder.record(200, Labels.of("Key", "SomeWork"));
 
             TimeUnit.SECONDS.sleep(1);
         }
